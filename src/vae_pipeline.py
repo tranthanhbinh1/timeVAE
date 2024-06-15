707d3936170fff5d5d7a0977923cb8a4b79bb034
tranthanhbinh1 <tranngocthanhbinh01@gmail.com>
2024-06-15 15:10:35 +0700

    update

--- src/vae_pipeline.py
+++ src/vae_pipeline.py
@@ -56,7 +56,7 @@ def run_vae_pipeline(dataset_name: str, vae_type: str):
     train_vae(
         vae=vae_model,
         train_data=scaled_train_data,
-        max_epochs=1000,
+        max_epochs=200,
         verbose=1,
     )
 
@@ -132,7 +132,7 @@ def run_vae_pipeline(dataset_name: str, vae_type: str):
 
 if __name__ == "__main__":
     # check `/data/` for available datasets
-    dataset = "boiler_train"
+    dataset = "sine_subsampled_train_perc_20"
 
     # models: vae_dense, vae_conv, timeVAE
     model_name = "timeVAE"
